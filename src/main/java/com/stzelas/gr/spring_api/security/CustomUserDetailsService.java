2b25e075df6ea4eacbed2c916858ce455cc5712a
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-25 18:32:23 +0300

    feat: Custom user detail service added

---
+++ src/main/java/com/stzelas/gr/spring_api/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.stzelas.gr.spring_api.security;
+
+import com.stzelas.gr.spring_api.models.User;
+import com.stzelas.gr.spring_api.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if (user == null) throw new UsernameNotFoundException("This user does not exist in the database");
+        return new UserPrincipal(user);
+    }
+}
