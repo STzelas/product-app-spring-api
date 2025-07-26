521ae0efc469846d93a3d33e49759eb5dcc1aa72
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:37:01 +0300

    feat: App Object Not Authorized exception added

---
+++ src/main/java/com/stzelas/gr/spring_api/core/exceptions/AppObjectNotAuthorizedException.java
@@ -0,0 +1,10 @@
+package com.stzelas.gr.spring_api.core.exceptions;
+
+public class AppObjectNotAuthorizedException extends AppGenericException {
+
+    private static final String DEFAULT_CODE = "NotAuthorized";
+
+    public AppObjectNotAuthorizedException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
