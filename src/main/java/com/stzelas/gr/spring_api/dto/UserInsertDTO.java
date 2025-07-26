75c366072b2ca5c60e255ceb0b5902cfabc3bb05
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:38:45 +0300

    feat: User Insert dto added

---
+++ src/main/java/com/stzelas/gr/spring_api/dto/UserInsertDTO.java
@@ -0,0 +1,31 @@
+package com.stzelas.gr.spring_api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserInsertDTO {
+    @NotEmpty(message = "First name is required")
+    private String firstname;
+
+    @NotEmpty(message = "Last name is required")
+    private String lastname;
+
+    @Email(message = "Invalid username")
+    private String username;
+
+    @Pattern(regexp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\\d)(?=.*?[@#$!%&*]).{8,}$",
+            message = "Invalid Password")
+    private String password;
+
+}
