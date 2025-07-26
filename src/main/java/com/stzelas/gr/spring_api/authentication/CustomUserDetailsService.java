2f26eb68b90816dfdb29f0f249cd4ea4361bf3ad
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:37:53 +0300

    feat: logic updated

--- src/main/java/com/stzelas/gr/spring_api/authentication/CustomUserDetailsService.java
+++ src/main/java/com/stzelas/gr/spring_api/authentication/CustomUserDetailsService.java
@@ -1,26 +1,23 @@
-package com.stzelas.gr.spring_api.security;
+package com.stzelas.gr.spring_api.authentication;
 
 import com.stzelas.gr.spring_api.model.User;
 import com.stzelas.gr.spring_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-
-        if (user == null) throw new UsernameNotFoundException("This user does not exist in the database");
-        return new UserPrincipal(user);
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
     }
+
 }
