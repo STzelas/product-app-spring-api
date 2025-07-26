adcfe68340c912835324cf6a878afc7adbf3b456
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:39:17 +0300

    feat: Optional instead of Object User added

--- src/main/java/com/stzelas/gr/spring_api/repositories/UserRepository.java
+++ src/main/java/com/stzelas/gr/spring_api/repositories/UserRepository.java
@@ -2,7 +2,12 @@ package com.stzelas.gr.spring_api.repositories;
 
 import com.stzelas.gr.spring_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+    Optional<User> findByUsername(String username);
 }
