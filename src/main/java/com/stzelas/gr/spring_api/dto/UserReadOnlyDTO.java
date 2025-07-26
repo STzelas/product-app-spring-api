a8e3168a151e7629efa9330a0c2cba3ffa41def0
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:39:01 +0300

    feat: User Read only dto added

---
+++ src/main/java/com/stzelas/gr/spring_api/dto/UserReadOnlyDTO.java
@@ -0,0 +1,15 @@
+package com.stzelas.gr.spring_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserReadOnlyDTO {
+    private String firstname;
+    private String lastname;
+}
