f6905f55db9b49d2bb47a7c8d9a5ea5eb60852f9
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 14:41:22 +0300

    featL User registration dto added

---
+++ src/main/java/com/stzelas/gr/spring_api/dto/UserRegistrationDTO.java
@@ -0,0 +1,28 @@
+package com.stzelas.gr.spring_api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationDTO {
+
+    @NotNull(message = "Username is required")
+    @Min(value = 3, message = "Username must be at least 3 characters")
+    private String username;
+
+    @NotNull(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotNull(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotNull(message = "Confirm Password is required")
+    private String confirmPassword;
+}
