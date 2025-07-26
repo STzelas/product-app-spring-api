77f62f9765a971fc03d6b9463e213e4568da7bca
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-26 15:36:54 +0300

    feat: App Object Invalid Argument exception added

---
+++ src/main/java/com/stzelas/gr/spring_api/core/exceptions/AppObjectInvalidArgumentException.java
@@ -0,0 +1,10 @@
+package com.stzelas.gr.spring_api.core.exceptions;
+
+public class AppObjectInvalidArgumentException extends AppGenericException {
+    private static final String DEFAULT_CODE = "InvalidArgument";
+
+    public AppObjectInvalidArgumentException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
+
