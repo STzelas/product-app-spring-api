a073ee2f638f46fe745de10f51c0c2b69c9a5673
Sot. Tzelatidis <stzelati@gmail.com>
2025-07-28 18:57:01 +0300

    feat: User correctly implements UserDetails

--- src/main/java/com/stzelas/gr/spring_api/authentication/CustomUserDetailsService.java
+++ src/main/java/com/stzelas/gr/spring_api/authentication/CustomUserDetailsService.java
@@ -17,7 +17,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
     }
 
 }
--- src/main/java/com/stzelas/gr/spring_api/model/User.java
+++ src/main/java/com/stzelas/gr/spring_api/model/User.java
@@ -2,10 +2,15 @@ package com.stzelas.gr.spring_api.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,5 +21,30 @@ public class User {
     @Column(unique = true)
     private String username;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 //    private String passwordHash;
 }
